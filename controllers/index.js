d72ac9cd9f1464f6feb16757fa7a3d17f6fea872
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-05-22 10:12:42 -0700

    added handlebars template engine and main/homepage views

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
+const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 
-// send 404 status if endpoint does not exist
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
+// send 404 status if endpoint does not exist
 router.use((req, res) => {
   res.status(404).end();
 });
--- server.js
+++ server.js
@@ -1,15 +1,33 @@
+// require dependencies
 const express = require('express');
-// const controllers = require('./controllers');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const controllers = require('./controllers');
+
+// instantiate the server
 const app = express();
+// creating a port for the server
 const PORT = process.env.PORT || 3333;
 
+// sequelize connection
 const sequelize = require('./config/connection');
 
+const hbs = exphbs.create({});
+
+// handlebars template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// express middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// use controllers
+app.use(controllers);
 
+// sync sequelize tables if true, then start server connection
 sequelize.sync({force: false})
 .then(() => {
     app.listen(PORT, () => console.log(`Now listening on Port ${PORT}!`));
